7f9e105de0c7f81618bf7c536813a7ffaf6e1b6e
Md Shakil Hossain <shakilshd23@gmail.com>
2025-03-22 12:06:53 +0600

    Brand Logo section added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Hero from "@/components/Home/Hero";
+import Brand from "@/components/Home/Brand";
 
 export default function Home() {
   return (
     <div>
 
       <Hero /> 
+      <Brand />
     
     </div>
   );
---
+++ components/Home/Brand.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Brand = () => {
+    return (
+        <div className="container mx-auto px-4">
+            <div className='flex flex-wrap items-center justify-center xl:justify-between py-10'>
+                <img className='m-4' src="/images/brand/b1.png" alt="BrandLogo" />
+                <img className='m-4' src="/images/brand/b2.png" alt="BrandLogo" />
+                <img className='m-4' src="/images/brand/b3.png" alt="BrandLogo" />
+                <img className='m-4' src="/images/brand/b4.png" alt="BrandLogo" />
+                <img className='m-4' src="/images/brand/b5.png" alt="BrandLogo" />
+            </div>
+        </div>
+    );
+};
+
+export default Brand;
